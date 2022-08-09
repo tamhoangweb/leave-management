4fec18d4f752afa9f2920c1df214eff999f82455
tamhoangweb <tamhoangweb@gmail.com>
2022-08-09 17:44:07 +0700

    Page + Page.Content

---
+++ src/modules/common/components/Page/Page.stories.jsx
@@ -0,0 +1,61 @@
+import GlobalStyle from '../GlobalStyle';
+import { Header } from '../Header';
+import Page from './Page';
+import { Home, HomeFilled, Books, BooksFilled, Clipboard, ClipboardFilled, Cog, CogFilled } from '../Icons';
+import { Sidebar } from '../Sidebar';
+const menus = [
+    {
+        path: '#',
+        activeIcon: HomeFilled,
+        icon: Home,
+        title: 'Home',
+    },
+    {
+        path: '#',
+        activeIcon: ClipboardFilled,
+        icon: Clipboard,
+        title: 'Leave Management',
+    },
+    {
+        path: '#',
+        activeIcon: BooksFilled,
+        icon: Books,
+        title: 'Document',
+    },
+    {
+        path: '#',
+        activeIcon: CogFilled,
+        icon: Cog,
+        title: 'Settings',
+    },
+];
+export default {
+    title: 'Example/Page',
+    component: Page,
+};
+
+const Template = () => (
+    <>
+        <GlobalStyle />
+        <Header fixed={true} />
+        <Page>
+            <Sidebar fixed={true}>
+                {menus.map(({ path, activeIcon, icon, title }, index) => {
+                    return (
+                        <Sidebar.Item
+                            href={path}
+                            ActiveIcon={activeIcon}
+                            Icon={icon}
+                            active={index === 1}
+                            key={index}
+                            title={title}
+                        />
+                    );
+                })}
+            </Sidebar>
+
+            <Page.Content>Page Content</Page.Content>
+        </Page>
+    </>
+);
+export const Default = Template.bind({});
--- src/modules/common/components/Sidebar/Sidebar.jsx
+++ src/modules/common/components/Sidebar/Sidebar.jsx
@@ -8,6 +8,19 @@ const StyledSidebar = styled.nav`
     min-height: calc(100vh - 3.5rem);
     width: 13.75rem;
     background-color: ${colors.tealDark};
+
+    ${({ fixed }) =>
+        fixed
+            ? `
+        position: fixed;
+        top: 3.5rem;
+        left: 0;
+        right: 0;
+        z-index: 2;
+        `
+            : `
+        position: absolute;
+        `}
 `;
 const Sidebar = (props) => <StyledSidebar {...props} />;
 Sidebar.Item = Item;
